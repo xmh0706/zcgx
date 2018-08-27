0d15298e2a8aa88de8e31cbc39f7548d80cd9c34
xmh0706 <1148538989@qq.com>
2018-08-27 23:23:46 +0800

    123

--- js/index.js
+++ js/index.js
@@ -13,12 +13,37 @@ function preArrow(){
 }//上一页
 function nextArrow(){
 	flash++;
-	flash =(flash==6)?5:flash;
+	flash =(flash==7)?6:flash;
 	chooseATTR()
 }//下一页
 // setInterval(function(){nextArrow()},1000)
 // 
-// 
+
+
+//
+
+function setVideo () {
+	var vHeight= document.getElementById('page_01').offsetHeight
+	var vWidth = document.getElementById('page_01').offsetWidth
+	if (vWidth*9 > vHeight*16) {
+		var top = (vWidth/16*9 - vHeight)/2
+		// console.log(top)
+		// document.getElementById("indexVideo").style.top = -top +"PX"
+		$("#indexVideo").css("top", -top + "px")
+		$("#indexVideo").css("width", vWidth + "px")
+		$("#indexVideo").css("left", 0)
+	}else {
+		var left = (vHeight*16/9 - vWidth)/2
+		var vWidthChange = vHeight/9*16
+		// console.log(vWidthChange)
+		$("#indexVideo").css("top", 0)
+		$("#indexVideo").css("width", vWidthChange + "px")
+		$("#indexVideo").css("left", -left + "px")
+	}
+}//控制底部视频大小
+///
+///
+
 $(document).ready(function(){
 	$("#bannerContent .button").click(function(){
 		$("#bannerContent .button .hideImg").css("display","block")
@@ -42,7 +67,7 @@ $(document).ready(function(){
 				$('html,body').animate({scrollTop:0 }, 300)
 				return false
 			}else{
-				if (flash < 5) {
+				if (flash < 6) {
 					nextArrow()
 					$('html,body').animate({scrollTop:0 }, 300)
 					return false
@@ -51,12 +76,9 @@ $(document).ready(function(){
 				}
 			}
 		}else{
-			if (flash != 5) {
-				return false
-			}
+			return false
 		}
-	});
-	//滚轮切换
+	});//滚轮切换
 	
 	var click = 0
 	$(".menuIcon").click(function(){
@@ -69,8 +91,7 @@ $(document).ready(function(){
 			$(".menuIcon").css("transform","none")
 			click = 0
 		}	
-	});
-	//响应式布局menu变化
+	});//响应式布局menu变化
 
 	$("#bannerContent").mousedown(function(event){
 		var downY = event.originalEvent.clientY
@@ -96,14 +117,16 @@ $(document).ready(function(){
 			$('html,body').animate({scrollTop:0 }, 500);
 		}
 		chooseATTR()
-	})
-	//鼠标按住滑动界面
+	})//鼠标按住滑动界面
 	
+	setVideo ()//视频大小控制函数
 })
 // 
 $(window).resize(function(){
 	var height= document.getElementById("banner").offsetHeight
 	$('#bannerContent').css("transition", "none");
 	$('#bannerContent').css("top", -height*flash + "px");
+	setVideo ()//视频大小控制函数
+
 	//浏览器大小变化时触发
 })
