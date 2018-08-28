8efccef8e0e7c18637b0aa85333de36b9056f9a4
xmh0706 <1148538989@qq.com>
2018-08-28 15:36:26 +0800

    1

--- js/index.js
+++ js/index.js
@@ -123,11 +123,11 @@ $(document).ready(function(){
 	$("#bannerContent").on("touchstart",function(e){
 	    startY = e.originalEvent.changedTouches[0].pageY;
 	    moveY = 0 
-	    if (flash == 6) {
-	    	$("html").css("overflow","auto")
-	    }else (
-	    	$("html").css("overflow","hidden")
-	    )
+	    // if (flash == 6) {
+	    // 	$("html").css("overflow","auto")
+	    // }else (
+	    // 	$("html").css("overflow","hidden")
+	    // )
 	    // console.log(e)
 	    // console.log(startY)
 	});
