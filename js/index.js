004779417ea380a13fb0384550dc469d73fde80a
xmh0706 <1148538989@qq.com>
2018-08-28 17:40:28 +0800

    1

--- js/index.js
+++ js/index.js
@@ -123,15 +123,14 @@ $(document).ready(function(){
 	$("#bannerContent").on("touchstart",function(e){
 	    startY = e.originalEvent.changedTouches[0].pageY;
 	    moveY = 0 
-	    if (flash == 6) {
-	    	$("html").css("overflow","auto")
-	    	$("body").css("overflow","auto")
-	    }else {
-	    	$('html,body').animate({scrollTop:0 }, 500);
-	    	$("html").css("overflow","hidden")
-	    	$("body").css("overflow","hidden")
-
-	    }
+	    // if (flash == 6) {
+	    // 	$("html").css("overflow","auto")
+	    // 	$("body").css("overflow","auto")
+	    // }else {
+	    // 	$('html,body').animate({scrollTop:0 }, 500);
+	    // 	$("html").css("overflow","hidden")
+	    // 	$("body").css("overflow","hidden")
+	    // }
 	    // console.log(e)
 	    // console.log(startY)
 	});
